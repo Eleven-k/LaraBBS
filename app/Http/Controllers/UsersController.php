8fb58a0c74b272f107725a2aabd2aca5c97dcb4d
Eleven-k <Twinwik@qq.com>
2020-08-19 22:12:10 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
@@ -15,9 +16,15 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    // 编辑资料
+    // 编辑资料页面
     public function edit(User $user)
     {
         return view('users.edit',compact('user'));
+    }
+
+    // 编辑资料逻辑
+    public function update(UserRequest $requests, User $user)
+    {
+
     }
 }
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,1 +1,39 @@
-11111
+@extends('layouts.default')
+@section('title','全部-ROCBOSS')
+@section('content')
+<div class="container">
+    <div class="col-md-8 offset-md-2">
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    <i class="glyphicon glyphicon-edit"></i> 编辑个人资料
+                </h4>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
+                    {{ method_field('PATCH') }}
+                    {{ csrf_field() }}
+
+                    @include('shared._error')
+
+                    <div class="form-group">
+                        <label htmlFor="name-field">用户名</label>
+                        <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
+                    </div>
+                    <div class="form-group">
+                        <label htmlFor="email-field">邮 箱</label>
+                        <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
+                    </div>
+                    <div class="form-group">
+                        <label htmlFor="introduction-field">个人简介</label>
+                        <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+                    </div>
+                    <div class="well well-sm">
+                        <button type="submit" class="btn btn-primary">保存</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
