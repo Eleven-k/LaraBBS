bacf8323c7c25e5682e30cfb2b67213a480347ad
Eleven-k <Twinwik@qq.com>
2020-08-21 01:07:09 +0800

    app.js更改

--- public/js/app.js
+++ public/js/app.js
@@ -37353,10 +37353,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\work\www\larabbs\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! F:\work\www\larabbs\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! F:\work\www\larabbs\resources\sass\before.scss */"./resources/sass/before.scss");
-module.exports = __webpack_require__(/*! F:\work\www\larabbs\resources\sass\after.scss */"./resources/sass/after.scss");
+__webpack_require__(/*! D:\Desktop\larabbs\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\Desktop\larabbs\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Desktop\larabbs\resources\sass\before.scss */"./resources/sass/before.scss");
+module.exports = __webpack_require__(/*! D:\Desktop\larabbs\resources\sass\after.scss */"./resources/sass/after.scss");
 
 
 /***/ })
